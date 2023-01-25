001f648367f54f653372e8f3e21393cb26364824
Samantha Davenport <sdavenport@students.mchenry.edu>
2023-01-24 21:13:08 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    var myByte: Byte = 3
+    var myShort: Short = 303
+    var myInt = 33
+    var myLong: Long = 333L
+    var myFloat: Float = 3.03F
+    var myDouble: Double = 303.303303
+    var myBoolean: Boolean = true
+    myBoolean = false
+    var myChar: Char = 'S'
+
+    println("Byte demo: $myByte")
+    println("Short demo: $myShort")
+    println("Int demo: $myInt")
+    println("Long demo: $myLong")
+    println("Float demo: $myFloat")
+    println("Double demo: $myDouble")
+    println("Boolean demo: $myBoolean")
+    println("Char demo: $myChar")
+}
